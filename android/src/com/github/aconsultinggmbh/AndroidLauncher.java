433a3efb333f37766e12cca27402a090dc5d250b
frahohen <franzho@edu.aau.at>
2017-03-21 10:30:22 +0100

    Initial commit

---
+++ android/src/com/github/aconsultinggmbh/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.aconsultinggmbh;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.aconsultinggmbh.ProjectY;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectY(), config);
+	}
+}
