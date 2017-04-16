0954d1d01f60aadb2f0818f487fc757c59600830
elmedinasaldic <elmedinasa@edu.aau.at>
2017-04-16 18:22:53 +0200

    Add Tests for items

---
+++ core/src/com/github/aconsultinggmbh/test/ItemTest.java
@@ -0,0 +1,70 @@
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.github.aconsultinggmbh.gameobject.GameObject;
+import com.github.aconsultinggmbh.gameobject.Item;
+import com.github.aconsultinggmbh.gameobject.ItemInvulnerability;
+import com.github.aconsultinggmbh.gameobject.Player;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ItemTest extends GameTest{
+    private Item item;
+    private ItemInvulnerability itemInvulnerability;
+    private ArrayList<GameObject> items;
+    private ArrayList<GameObject> players;
+    private String path = "core/src/com/github/aconsultinggmbh/test/resources/item.png";
+    private Player player;
+
+    @Before
+    public void setup() {
+        item = new Item(path, 0,0, "Item");
+        itemInvulnerability = new ItemInvulnerability(path, 0,0, "Item");
+
+        items = new ArrayList<GameObject>();
+        players = new ArrayList<GameObject>();
+
+        items.add(item);
+        items.add(itemInvulnerability);
+
+        player = new Player(path, 0,0, "Player");
+        players.add(player);
+    }
+
+    @Test
+    public void testCollideWithItem(){ //item with item
+        Item newItem = new Item(path, 0,0, "NewItem");
+
+        assertEquals(
+                item.getName(),
+                newItem.collideWithObject(items)
+        );
+        assertEquals(
+                itemInvulnerability.getName(),
+                newItem.collideWithObject(items)
+        );
+    }
+
+    @Test
+    public void testCollideWithPlayer (){ //player mit item
+        Item newItem = new Item(path, 0,0, "NewItem");
+
+        assertEquals(
+                player.getName(),
+                newItem.collideWithObject(players)
+        );
+    }
+
+    //Gegner mit item ??
+    //FloorMap und items?
+
+
+
+}
