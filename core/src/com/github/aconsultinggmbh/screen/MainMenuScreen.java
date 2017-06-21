271c1a8c8d93062f40455c99a677a36ecf626a92
maile <maingo@edu.aau.at>
2017-06-21 14:32:41 +0200

    Herzschlag rausgenommen

--- core/src/com/github/aconsultinggmbh/screen/MainMenuScreen.java
+++ core/src/com/github/aconsultinggmbh/screen/MainMenuScreen.java
@@ -2,16 +2,10 @@ package com.github.aconsultinggmbh.screen;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Net;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.net.ServerSocket;
-import com.badlogic.gdx.net.ServerSocketHints;
-import com.badlogic.gdx.net.Socket;
-import com.badlogic.gdx.net.SocketHints;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -34,7 +28,7 @@ public class MainMenuScreen implements Screen {
     private Sound menuSound;
 
     public MainMenuScreen(ProjectY screenManager) {
-        menuSound=Gdx.audio.newSound(Gdx.files.internal("heartbeat.wav"));
+       menuSound=Gdx.audio.newSound(Gdx.files.internal("heartbeat.wav"));
         this.screenManager = screenManager;
         create();
     }
@@ -102,7 +96,7 @@ public class MainMenuScreen implements Screen {
         //table.debug();
         stage.addActor(table);
         Gdx.input.setInputProcessor(stage);
-        menuSound.loop(1f);
+      //  menuSound.loop(1f);
     }
 
     @Override
